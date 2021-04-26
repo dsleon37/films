f4a43ae085715f2d7c5539e82870e799b425b378
dsleon37 <dsleon@ibm.com>
2021-04-26 12:30:11 -0500

    Creacion de backend Springboot inicializado

---
+++ springboot-films/src/main/java/com/films/springbootfilms/SpringbootFilmsApplication.java
@@ -0,0 +1,13 @@
+package com.films.springbootfilms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootFilmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootFilmsApplication.class, args);
+	}
+
+}
