a0f88666dbfac050950f88b5771e035a4ba97ad1
dsleon37 <dsleon@ibm.com>
2021-04-26 16:24:14 -0500

    Config Inicial

---
+++ spring-boot-films/src/main/java/com/films/springboot/films/entity/FilmList.java
@@ -0,0 +1,11 @@
+package com.films.springboot.films.entity;
+
+import lombok.Data;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="")
+@Data
+public class FilmList {
+}
