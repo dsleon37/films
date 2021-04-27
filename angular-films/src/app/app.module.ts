05410c32db72fd786872defc5e50828bd5e26fb7
LorenaForero04 <lorena.forero0411@gmail.com>
2021-04-27 07:05:41 -0500

    Creación alta de película

--- angular-films/src/app/app.module.ts
+++ angular-films/src/app/app.module.ts
@@ -1,15 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { Routes, RouterModule, Router} from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
+import { AltaPeliculaComponent } from './controlador/pelicula/alta-pelicula/alta-pelicula.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: '/peliculas', pathMatch: 'full'},
+  {path: '**', redirectTo: '/peliculas', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AltaPeliculaComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
