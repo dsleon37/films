d6256cb76ef7f9dfb76bd7e6e721b728624bc55d
djgarciab <djgarciab@hotmail.com>
2021-04-28 08:16:21 -0500

    Componente Alta-Datos-Pelicula
    Componente para caso de uso: Alta de datos necesarios de las películas

--- angular-films/src/app/app.module.ts
+++ angular-films/src/app/app.module.ts
@@ -9,16 +9,20 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AltaPeliculaComponent } from './controlador/pelicula/alta-pelicula/alta-pelicula.component';
+import { AltaDatosPeliculaComponent } from './controlador/pelicula/alta-datos-pelicula/alta-datos-pelicula.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: '/peliculas', pathMatch: 'full'},
-  {path: '**', redirectTo: '/peliculas', pathMatch: 'full'}
+  //{path: '', redirectTo: '/peliculas', pathMatch: 'full'},
+  //{path: '**', redirectTo: '/peliculas', pathMatch: 'full'}
+
+  { path : 'datos-pelicula', component: AltaDatosPeliculaComponent},
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    AltaPeliculaComponent
+    AltaPeliculaComponent,
+    AltaDatosPeliculaComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
