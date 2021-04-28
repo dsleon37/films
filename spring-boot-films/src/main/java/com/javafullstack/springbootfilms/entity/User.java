9a58a8d1e7d5e05875db1b575e89d66316cf94f5
Yilber-Salazar-24 <yilber.salazar@ibm.com>
2021-04-27 22:33:33 -0500

    correccion del backend y Api user
    se corrigio el error a la conexion a la base de datos y se creo el repositorio de user

---
+++ spring-boot-films/src/main/java/com/javafullstack/springbootfilms/entity/User.java
@@ -0,0 +1,39 @@
+package com.javafullstack.springbootfilms.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="user")
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_role_id", nullable = false)
+    private UserRole role;
+
+    @Column(name = "username")
+    private String userName;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "email")
+    private String email;
+
+
+}
