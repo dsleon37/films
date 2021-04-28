d5c3f1fc3426f088abaadc8636f531eceef25f2d
LorenaForero04 <lorena.forero0411@gmail.com>
2021-04-27 22:18:24 -0500

    Catálogo películas

---
+++ angular-films/src/app/controlador/pelicula/pelicula.ts
@@ -0,0 +1,7 @@
+export class Pelicula {
+    id: number = 0;
+    title: string = '';
+    description: string = '';
+    date: Date | undefined;
+    imageUrl: string = '';
+}
--- spring-boot-films/src/main/java/com/films/springboot/films/entity/Film.java
+++ spring-boot-films/src/main/java/com/films/springboot/films/entity/Film.java
@@ -1,11 +1,22 @@
 package com.films.springboot.films.entity;
 
 import lombok.Data;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 @Table(name="film")
 @Data
 public class Film {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    private Date date;
 }
