207c05e75d85d0ce4fa522570fa212724d03c06f
djgarciab <djgarciab@hotmail.com>
2021-04-28 18:21:38 -0500

    Add service to alta datos peliculas

--- angular-films/src/app/controlador/pelicula/alta-datos-pelicula/alta-datos-pelicula.component.ts
+++ angular-films/src/app/controlador/pelicula/alta-datos-pelicula/alta-datos-pelicula.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { ServicioPeliculaService } from 'src/app/modelo/pelicula/servicio-pelicula.service';
+import { Categoria } from '../categoria';
+import { Actor } from '../actor';
+import { Director} from '../director';
 
 @Component({
   selector: 'app-alta-datos-pelicula',
@@ -11,14 +15,18 @@ export class AltaDatosPeliculaComponent implements OnInit {
 
   datosPeliculaFormGroup: FormGroup;
   
-  actors:string[]=['actor1','actor2','actor3','actor4','actor5'];
+  //actors:string[]=['actor1','actor2','actor3','actor4','actor5'];
+  //directors:string[]=['director1','director2','director3','director4','director5'];
+  // categories:string[]=['category1','category2','category3','category4','category5'];
 
-  directors:string[]=['director1','director2','director3','director4','director5'];
+  categories: Categoria[] = [];
+  actors: Actor[] = [];
+  directors: Director[] = [];
 
-  categories:string[]=['category1','category2','category3','category4','category5'];
-  
+  constructor(private formBuilder: FormBuilder, 
+              private router: Router, 
+              private servicioPelicula: ServicioPeliculaService) {
 
-  constructor(private formBuilder: FormBuilder, router: Router) {
     this.datosPeliculaFormGroup = this.formBuilder.group({
       film: this.formBuilder.group({
         title: new FormControl(''),
@@ -34,8 +42,33 @@ export class AltaDatosPeliculaComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.getCategories()
+  }
 
-    // this.datosPeliculaFormGroup = this.formBuilder.group( {} )
+  getCategories() {
+    this.servicioPelicula.getCategories().subscribe(
+      data => {
+        console.log('Categorias: ' + JSON.stringify(data));
+        //this.categories = data;
+      }
+    )
+  }
+  getActors() {
+    this.servicioPelicula.getActors().subscribe(
+      data => {
+        console.log('Actores: ' + JSON.stringify(data));
+        
+      }
+    )
+  }
+
+  getDirectors() {
+    this.servicioPelicula.getDirectors().subscribe(
+      data => {
+        console.log('Directores: ' + JSON.stringify(data));
+        
+      }
+    )
   }
   onSubmit() {
     console.log('Handling the submit button');
