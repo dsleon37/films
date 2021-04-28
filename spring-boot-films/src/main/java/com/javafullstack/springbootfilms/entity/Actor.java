531a82e34b20d71e67681b80cac79559dce0d4e5
LorenaForero04 <lorena.forero0411@gmail.com>
2021-04-28 15:32:07 -0500

    Creación spring Entity pelicula,categoria, actor y director

--- spring-boot-films/src/main/java/com/javafullstack/springbootfilms/entity/Actor.java
+++ spring-boot-films/src/main/java/com/javafullstack/springbootfilms/entity/Actor.java
@@ -1,4 +1,23 @@
 package com.javafullstack.springbootfilms.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name="actor")
+@Getter
+@Setter
 public class Actor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "actor")
+    private Set<Film> films;
 }
--- spring-boot-films/src/main/java/com/javafullstack/springbootfilms/entity/Category.java
+++ spring-boot-films/src/main/java/com/javafullstack/springbootfilms/entity/Category.java
@@ -1,4 +1,24 @@
 package com.javafullstack.springbootfilms.entity;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name="category")
+@Getter
+@Setter
 public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
+    private Set<Film> films;
 }
