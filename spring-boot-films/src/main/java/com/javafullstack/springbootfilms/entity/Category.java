0d0de97300134d08f55bb6dcf3f06476dfbd5bb1
LorenaForero04 <lorena.forero0411@gmail.com>
2021-04-30 22:52:16 -0500

    Modificar y filtro de pelicula

--- spring-boot-films/src/main/java/com/javafullstack/springbootfilms/entity/Category.java
+++ spring-boot-films/src/main/java/com/javafullstack/springbootfilms/entity/Category.java
@@ -15,8 +15,10 @@ public class Category {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
     private Long id;
 
+    @Column(name="name")
     private String name;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
