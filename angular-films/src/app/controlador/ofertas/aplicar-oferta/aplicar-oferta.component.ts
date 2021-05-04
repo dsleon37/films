d1294e100ba98a1730f0bd57804a079ad91c7a63
Pierina-Fierro <pierina.fierro-c@ibm.com>
2021-05-04 12:14:16 -0500

    fix

--- angular-films/src/app/controlador/ofertas/aplicar-oferta/aplicar-oferta.component.ts
+++ angular-films/src/app/controlador/ofertas/aplicar-oferta/aplicar-oferta.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscriber } from 'rxjs';
 import { OfertasService } from 'src/app/modelo/ofertas/ofertas.service';
-import { CarroOfertas } from '../common/carro-ofertas';
 import { Oferta } from '../common/oferta';
+import { Suscriptor } from '../common/suscriptor';
 
 @Component({
   selector: 'app-aplicar-oferta',
@@ -11,37 +14,60 @@ import { Oferta } from '../common/oferta';
 
 export class AplicarOfertaComponent implements OnInit {
 
-  offers: Oferta[] = [];
+  aplicarOfertaFormGroup: FormGroup;
+  offers: Oferta = new Oferta;
   totalPoints: number = 0;
-  totalOffers: number = 0;
   OfferId: number = 0;
+  currentOfferId: number;
+  oferta: Oferta;
+  subscriber: Suscriptor = new Suscriptor;
+  suscriptor: Suscriptor;
+  mensaje: string;
+  mensajeErr: string;
 
-  constructor(private ofertasService: OfertasService) { }
+  constructor(private ofertasService: OfertasService, private route: ActivatedRoute, private formBuilder: FormBuilder, private router: Router) { 
 
-  ngOnInit(): void {
-    this.listOffers();
+    this.aplicarOfertaFormGroup= this.formBuilder.group({
+      newSuscriptor: this.formBuilder.group({
+        id: new FormControl(''),
+        points: new FormControl('')
+      })
+    })
   }
 
-  listOffers() {
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(() => {
+      this.getOffers();
+    })
+  }
 
-    // get a handle to the offers
-    this.offers = this.ofertasService.offers;
+  getOffers() {
+    
+    const currentOfferId: number = +this.route.snapshot.paramMap.get('id');
 
-    // subscribe to the cart totalPoints
-
-    this.ofertasService.totalPoints.subscribe(
-      (data: number) => this.totalPoints = data
-    );
-
-    this.ofertasService.getOffersList().subscribe(
+    this.ofertasService.getOffers(currentOfferId).subscribe(
       data => {
         this.offers = data;
       }
     )
-
-    // addToCart(CarroOfertas: Oferta) {
-    //   console.log(`Adding offer: ${CarroOfertas.description}, ${CarroOfertas.addedPoints}, ${CarroOfertas.subbedPoints}`)
-    // }
   }
 
+  onSubmit(){
+
+    if(this.aplicarOfertaFormGroup.invalid){
+      this.aplicarOfertaFormGroup.markAllAsTouched();
+    }else{
+      this.suscriptor = this.aplicarOfertaFormGroup.get('newSuscriptor').value
+      this.suscriptor.user = "http://localhost:8080/api/user/3";
+      console.log('suscriptor', this.suscriptor);
+      this.ofertasService.aplicarOffer(this.suscriptor).subscribe({
+        next: response =>{
+          this.mensaje = "Se ha registrado correctamente la oferta.";
+        },
+        error: err => {
+          this.mensajeErr = `Error al registrar oferta: `+err.message;
+        }
+      });
+    }
+  }
 }
