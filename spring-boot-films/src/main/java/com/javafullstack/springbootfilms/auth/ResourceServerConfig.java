7d6a4b0f4e10891c7826df754c9033683a75c7ef
LorenaForero04 <lorena.forero0411@gmail.com>
2021-05-04 15:51:28 -0500

    ajustes

--- spring-boot-films/src/main/java/com/javafullstack/springbootfilms/auth/ResourceServerConfig.java
+++ spring-boot-films/src/main/java/com/javafullstack/springbootfilms/auth/ResourceServerConfig.java
@@ -22,9 +22,9 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers(HttpMethod.GET,"/api/films", "/api/categories", "api/films/**").permitAll()
-                //.antMatchers(HttpMethod.POST,"/api/users").permitAll()
-                //.antMatchers(HttpMethod.POST,"/api/films/**").permitAll()
-                //.antMatchers(HttpMethod.POST,"/api/offers").hasRole("cinema")
+                .antMatchers(HttpMethod.POST,"/api/users").permitAll()
+                .antMatchers(HttpMethod.POST,"/api/films/**").permitAll()
+                .antMatchers(HttpMethod.POST,"/api/offers").hasRole("cinema")
                 .anyRequest().permitAll()
                 .and().cors().configurationSource(corsConfigurationSource());
     }
