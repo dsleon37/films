6d02359a9b6c67ccdccf071f099902b778283103
LorenaForero04 <lorena.forero0411@gmail.com>
2021-05-04 11:50:08 -0500

    Update SpringBootFilmsApplication.java

--- spring-boot-films/src/main/java/com/javafullstack/springbootfilms/SpringBootFilmsApplication.java
+++ spring-boot-films/src/main/java/com/javafullstack/springbootfilms/SpringBootFilmsApplication.java
@@ -12,4 +12,14 @@ public class SpringBootFilmsApplication{
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootFilmsApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		String password="12345";
+
+		for(int i = 0; i< 4; i++){
+			String passwordBcrypt = passwordEncoder.encode(password);
+			System.out.println(passwordBcrypt);
+		}
+	}
 }
